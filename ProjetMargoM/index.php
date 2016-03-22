0e38c33bd50489268b67068ab8ee93708f46f429
stalaa <sammy.talaa@gmail.com>
2016-03-22 08:59:50 +0100

    First commit

---
+++ ProjetMargoM/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+  Template 0232
+  Designed by CSS Templates For Free and licensed under the Creative Commons Attribution 3.0
+  Website: http://www.csstemplatesforfree.org/
+  Twitter: @cssforfree
+--><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="content-type" content="text/html; charset=utf-8"><meta name="description" content=""><meta name="keywords" content=""><title>Your Page Name</title><link rel="stylesheet" type="text/css" href="site-assets/screen.css"></head><body>
+    <div id="outer-container">
+      <div id="header">
+        <div id="main-logo">
+          <h1><a href="#">Your Page Name</a></h1>
+        </div>
+        <div id="main-menu">
+          <ul><li class="first-item">
+              <a href="#">Page Link</a>
+            </li>
+            <li>
+              <a href="#">Page Link</a>
+            </li>
+            <li>
+              <a href="#">Page Link</a>
+            </li>
+            <li>
+              <a href="#">Page Link</a>
+            </li>
+            <li>
+              <a href="#">Page Link</a>
+            </li>
+            <li>
+              <a href="#">Page Link</a>
+            </li>
+            <li class="last-item">
+              <a href="#">Page Link</a>
+            </li>
+          </ul><br class="clear-all"></div>
+      </div>
+      <div id="inner-container">
+        <div id="column1">
+          <div id="content1">
+            <h2>Welcome To Your Page Name</h2>
+            <p>
+              Welcome. This is another free CSS template created by <a href="http://www.csstemplatesforfree.org/">CSS Templates For Free</a>. It is released under the <a href="http://www.csstemplatesforfree.org/license">Creative Commons Attribution 3.0</a> license which permits you to use it personally and commercially provided you retain the footer link back to our website. All photos used in our free CSS templates are public domain photos provided by <a href="http://www.pdphoto.org">PDPhoto.org</a>.
+            </p>
+          </div>
+          <div id="content2">
+            <h3>Subheading</h3>
+            <p>
+              Sapien lacus tempus tristique commodo. Commodo tempus volutpat pretium morbi. Cras integer fringilla nisi erat mauris. Odio adipiscing molestie accumsan. Ullamcorper facilisis ac enim ultrices. Pretium interdum magnis non malesuada sodales posuere sociis. Ullamcorper feugiat mollis eu pellentesque. Curae etiam diam dignissim.
+            </p>
+            <ul class="links"><li class="first-item">
+                <a href="#">Pretium malesuada molestie vestibulum ipsum mattis</a>
+              </li>
+              <li>
+                <a href="#">Congue tincidunt aliquam praesent id tempus justo</a>
+              </li>
+              <li>
+                <a href="#">Pretium scelerisque odio aliquam fringilla ultrices porta ligula</a>
+              </li>
+              <li>
+                <a href="#">Sollicitudin pulvinar orci adipiscing mollis semper montes</a>
+              </li>
+              <li class="last-item">
+                <a href="#">Faucibus arcu donec praesent nascetur rhoncus</a>
+              </li>
+            </ul></div>
+          <div id="content3">
+            <h3>Subheading</h3>
+            <p>
+              Scelerisque metus scelerisque placerat ullamcorper integer. Laoreet aliquam ante congue magnis mi donec. Vitae purus vitae mauris. Etiam nec diam vivamus consequat urna sagittis pulvinar. Hendrerit lacinia dolor tellus sapien. Felis lacus nisi erat nunc quam eget hendrerit. Purus fermentum viverra facilisis malesuada vestibulum lacus. Quis vestibulum cum aliquam felis eleifend.
+            </p>
+          </div>
+          <br class="clear-all"></div>
+        <div id="column2">
+          <h3>Subheading</h3>
+          <div class="news">
+            <ul class="links news"><li class="first-item">
+                <span class="date">11/28</span> <a href="#">Cras urna</a>
+              </li>
+              <li>
+                <span class="date">11/24</span> <a href="#">Velit arcu morbi</a>
+              </li>
+              <li class="last-item">
+                <span class="date">11/23</span> <a href="#">Ornare etiam magna</a>
+              </li>
+            </ul></div>
+          <h3>Subheading</h3>
+          <img class="float-top" src="site-assets/images/pic1.jpg" width="250" height="75" alt=""><p>
+            Enim fermentum luctus adipiscing. Fringilla sodales proin mollis massa. Urna eu velit lacinia nisl blandit fusce consectetur. Dictum elementum felis varius tempus nisi lacus.
+          </p>
+        </div>
+        <br class="clear-all"></div>
+    </div>
+    <div id="site-credits">
+      © Your Page Name. Design by <a href="http://www.csstemplatesforfree.org">CSS Templates For Free</a>.
+</div>
+  </body></html>
