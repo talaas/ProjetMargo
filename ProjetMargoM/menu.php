aa929dc81949a493b9c913f15f597fe5c1655855
stalaa <sammy.talaa@gmail.com>
2016-03-22 09:25:53 +0100

    Menu

---
+++ ProjetMargoM/menu.php
@@ -0,0 +1,20 @@
+        <div id="main-menu">
+          <ul><li class="first-item">
+                  <a href="index.php">Accueil</a>
+            </li>
+            <li>
+                <a href="Enseignements.php">Enseignements</a>
+            </li>
+            <li>
+              <a href="Filiere.php">Filières</a>
+            </li>
+            <li>
+              <a href="Classes.php">Classes</a>
+            </li>
+            <li>
+              <a href="Eleves.php">Elèves</a>
+            </li>
+            <li>
+              <a href="Connexion.php">Connexion</a>
+            </li>
+          </ul><br class="clear-all"></div>
